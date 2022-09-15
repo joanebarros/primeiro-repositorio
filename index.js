df833f8cfdce8886a854835d8ea64ac99e899a75
joanebarros <joane.barros@hotmail.com>
2022-09-15 18:08:29 -0300

    Adicionei o arquivo index.js e index.html

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("Hello word")
+console.log(5)
+console.log(43.76)
+console.log(true)
+console.log(false)
+
+let mensagem = "SoulCode BC 25 JFS"
+
+console.log(mensagem)
+
+mensagem = "Novo texto"
+
+console.log(mensagem) 
+
