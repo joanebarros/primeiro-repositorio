e573604730aa672f68c4dbfd59a8e2f16b6d9a49
joanebarros <joane.barros@hotmail.com>
2022-09-16 18:20:10 -0300

    Operadores de encrementamento e descrementamento

---
+++ operadores_de_incrementos.js
@@ -0,0 +1,14 @@
+alert( "codigo funcionando!")
+const nomeUsuario = prompt("Olà, usuario, qual o seu nome?")
+
+let num = Number(prompt("Olà, " + nomeUsuario + " Digite algum nùmero, por favor"))
+
+
+console.log(num++)
+console.log(num)
+console.log(++num)
+
+
+console.log(num--)
+console.log(num)
+console.log(--num)
